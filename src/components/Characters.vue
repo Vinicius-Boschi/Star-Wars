314aba77306b51e007e78cc2b88088a79c57ad8a
Vinicius Boschi <vinicius.yg@gmail.com>
2023-11-12 21:29:18 -0300

    Adicionando funcionalidade ao search e pagination

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -28,7 +28,9 @@ export default {
       currentUrl: "",
       complementUrl: "people",
       loading: true,
-      error404: "https://i.pinimg.com/originals/c0/6c/9d/c06c9d63bda3f0a823aee1b2f47b0457.png"
+      error404: "https://i.pinimg.com/originals/c0/6c/9d/c06c9d63bda3f0a823aee1b2f47b0457.png",
+      currentPage: 1,
+      totalPages: 9
     }
   },
   computed: {
@@ -37,27 +39,39 @@ export default {
     },
   },
   methods: {
-    fetchRequision() {
+    fetchRequision(pageNumber) {
       this.loading = true
-      let req = new Request(this.currentUrl + this.complementUrl)
+      this.heroes = []
+      let req = new Request(`${this.currentUrl}${this.complementUrl}?page=${pageNumber}`)
       fetch(req)
         .then((resp) => {
           return resp.json()
         })
         .then((characters) => {
-          characters.results.forEach(heroe => {
-            heroe.id = heroe.url.split('/')
-            .filter((part) => {
-              return !!part
-            }).pop()
-            this.heroes.push(heroe)
-          })
+          this.totalPages = Math.ceil(characters.count / 10)
+          // this.heroes = characters.results.map((heroe) => ({
+          //   ...heroe,
+          //   id: this.extractIdFromUrl(heroe.url),
+          // }))
+          if(characters.results) {
+            characters.results.forEach((heroe) => {
+              heroe.id = this.extractIdFromUrl(heroe.url)
+              this.heroes.push(heroe)
+            })
+          }
+          // characters.results.forEach(heroe => {
+          //   heroe.id = heroe.url.split('/')
+          //   .filter((part) => {
+          //     return !!part
+          //   }).pop()
+          //   this.heroes.push(heroe)
+          // })
           this.loading = false
         })
         .catch((error) => {
           console.log("Error fetching characters" ,error)
           this.loading = false
-        });
+        })
     },
     getImageUrl(id) {
       if(!id) {
@@ -71,10 +85,27 @@ export default {
     setHeroesUrl(url) {
       this.$emit("setHeroesUrl", url)
     },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+        this.fetchRequision(this.currentPage)
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--
+        this.fetchRequision(this.currentPage)
+      }
+    },
+    extractIdFromUrl(url) {
+      const parts = url.split("/").filter((part) => !!part)
+      return parts.pop()
+    },
   },
   created() {
     this.currentUrl = this.apiUrl
-    this.fetchRequision()
+    this.currentPage = 1
+    this.fetchRequision(this.currentPage)
   },
 }
 </script>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,17 +1,98 @@
 <template>
-   <div class="search">
-    <input type="text" placeholder="Pesquise pelo nome do personagem">
-        <div class="pagination">
-            <button class="pagination__button"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M17.77 3.77L16 2 6 12l10 10 1.77-1.77L9.54 12z"></path></svg></button>
-            <button class="active">1</button>
-            <button>2</button>
-            <button>3</button>
-            <button class="pagination__button"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M6.23 20.23L8 22l10-10L8 2 6.23 3.77 14.46 12z"></path></svg></button>
-        </div>
-   </div>
+  <div class="search">
+    <input v-model="searchQuery" type="text" placeholder="Pesquise pelo nome do personagem" />
+    <div class="pagination">
+      <button class="pagination__button" @click="prevPage" :disabled="currentPage === 1">
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path fill="none" d="M0 0h24v24H0z"></path>
+          <path d="M17.77 3.77L16 2 6 12l10 10 1.77-1.77L9.54 12z"></path>
+        </svg>
+      </button>
+      <button v-for="page in totalPages" :key="page" :class="{ active: page === currentPage}" @click="goToPage(page)">{{ page }}</button>
+      <button class="pagination__button" @click="nextPage" :disabled="currentPage === totalPages">
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height="1em"
+          width="1em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path fill="none" d="M0 0h24v24H0V0z"></path>
+          <path d="M6.23 20.23L8 22l10-10L8 2 6.23 3.77 14.46 12z"></path>
+        </svg>
+      </button>
+    </div>
+    <!-- <div class="list">
+    <ul>
+      <li v-for="character in characters" :key="character.name">
+        <span>{{ character.name }}</span>
+      </li>
+    </ul>
+  </div> -->
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+        searchQuery: '',
+        currentPage: 1,
+        totalPages: 9,
+        characters: []
+    }
+  },
+  watch: {
+    searchQuery: 'searchCharacters',
+    currentPage: 'searchCharacters',
+  },
+  methods: {
+    async searchCharacters() {
+        try {
+            const response = await fetch(
+            `https://swapi.dev/api/people/?search=${this.searchQuery}&page=${this.currentPage}`
+            )
+            const data = await response.json()
+
+            this.characters = []
+
+            this.characters = data.results
+            this.totalPages = Math.ceil(data.count / 10)
+        } catch (error) {
+            console.error('Failed to search the characters', error)
+        }
+    },
+    async prevPage() {
+        if (this.currentPage > 1) {
+            this.currentPage--
+            await this.searchCharacters()
+        }
+    },
+    async nextPage() {
+        if (this.currentPage < this.totalPages) {
+            this.currentPage++
+            await this.searchCharacters()
+        }
+    },
+    async goToPage(page) {
+        this.currentPage = page
+        await this.searchCharacters()
+    },
+  },
+}
+</script>
+
 <style lang="scss">
-    @import '../assets/scss/variables.scss';
-    @import '../assets/scss/styles/search.scss';
+    @import "../assets/scss/variables.scss";
+    @import "../assets/scss/styles/search.scss";
 </style>
