8b54ab17e84fc0291b56e75f225d18e51048f793
Vinicius-Boschi <vinicius.yg@gmail.com>
2026-05-23 20:35:20 -0300

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,21 @@
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import VitePages from 'vite-plugin-pages'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VitePages from "vite-plugin-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [  
-=======
-  plugins: [
->>>>>>> 76e237d54cf98249cefd13e80c326b480c9e13e3
-    vue(),
-    VitePages()
-  ],
+  plugins: [vue(), VitePages()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-<<<<<<< HEAD
-    css: {
+  css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@use "@/assets/scss/global.scss" as *;`,
       },
     },
-  },  
-=======
->>>>>>> 76e237d54cf98249cefd13e80c326b480c9e13e3
-})
+  },
+});
